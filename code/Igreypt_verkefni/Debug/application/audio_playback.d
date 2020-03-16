cb52e950da486a1e11cb45d73734d810a098e6f4
HjaltiThrastarson <62251615+HjaltiThrastarson@users.noreply.github.com>
2020-03-16 15:21:36 +0000

    Create audio_playback.d

---
+++ code/Igreypt_verkefni/Debug/application/audio_playback.d
@@ -0,0 +1,141 @@
+# FIXED
+
+application/audio_playback.obj: ../application/audio_playback.c
+application/audio_playback.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/stdlib.h
+application/audio_playback.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/_ti_config.h
+application/audio_playback.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/linkage.h
+application/audio_playback.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/sys/cdefs.h
+application/audio_playback.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/stdint.h
+application/audio_playback.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/_stdint40.h
+application/audio_playback.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/sys/stdint.h
+application/audio_playback.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/sys/_types.h
+application/audio_playback.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/machine/_types.h
+application/audio_playback.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/machine/_stdint.h
+application/audio_playback.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/sys/_stdint.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/driverlib.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/inc/hw_memmap.h
+application/audio_playback.obj: C:/ti/ccs930/ccs/ccs_base/msp430/include/msp430.h
+application/audio_playback.obj: C:/ti/ccs930/ccs/ccs_base/msp430/include/msp430fr5994.h
+application/audio_playback.obj: C:/ti/ccs930/ccs/ccs_base/msp430/include/in430.h
+application/audio_playback.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/intrinsics.h
+application/audio_playback.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/intrinsics_legacy_undefs.h
+application/audio_playback.obj: C:/ti/ccs930/ccs/ccs_base/msp430/include/legacy.h
+application/audio_playback.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/stdbool.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/adc12_b.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/aes256.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/comp_e.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/crc.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/crc32.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/cs.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/dma.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/esi.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/eusci_a_spi.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/eusci_a_uart.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/eusci_b_i2c.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/eusci_b_spi.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/framctl.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/framctl_a.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/gpio.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/lcd_c.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/mpu.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/mpy32.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/pmm.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/ram.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/ref_a.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/rtc_b.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/rtc_c.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/sfr.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/sysctl.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/timer_a.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/timer_b.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/tlv.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/wdt_a.h
+application/audio_playback.obj: ../application/audio_playback.h
+application/audio_playback.obj: ../application/dac8311.h
+application/audio_playback.obj: ../application/global.h
+application/audio_playback.obj: ../application/audio_collect.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/grlib/grlib.h
+application/audio_playback.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/assert.h
+application/audio_playback.obj: ../application/application.h
+application/audio_playback.obj: ../application/global.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/DSPLib/include/DSPLib.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/DSPLib/include/DSPLib_types.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/DSPLib/include/DSPLib_lea.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/DSPLib/include/DSPLib_support.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/DSPLib/include/DSPLib_vector.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/DSPLib/include/DSPLib_matrix.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/DSPLib/include/DSPLib_filter.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/DSPLib/include/DSPLib_transform.h
+application/audio_playback.obj: C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/DSPLib/include/DSPLib_utility.h
+application/audio_playback.obj: ../application/FFT.h
+application/audio_playback.obj: ../application/fir.h
+
+../application/audio_playback.c: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/stdlib.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/_ti_config.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/linkage.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/sys/cdefs.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/stdint.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/_stdint40.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/sys/stdint.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/sys/_types.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/machine/_types.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/machine/_stdint.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/sys/_stdint.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/driverlib.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/inc/hw_memmap.h: 
+C:/ti/ccs930/ccs/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccs930/ccs/ccs_base/msp430/include/msp430fr5994.h: 
+C:/ti/ccs930/ccs/ccs_base/msp430/include/in430.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/intrinsics.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/intrinsics_legacy_undefs.h: 
+C:/ti/ccs930/ccs/ccs_base/msp430/include/legacy.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/stdbool.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/adc12_b.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/aes256.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/comp_e.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/crc.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/crc32.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/cs.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/dma.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/esi.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/eusci_a_spi.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/eusci_a_uart.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/eusci_b_i2c.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/eusci_b_spi.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/framctl.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/framctl_a.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/gpio.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/lcd_c.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/mpu.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/mpy32.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/pmm.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/ram.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/ref_a.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/rtc_b.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/rtc_c.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/sfr.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/sysctl.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/timer_a.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/timer_b.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/tlv.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/driverlib/MSP430FR5xx_6xx/wdt_a.h: 
+../application/audio_playback.h: 
+../application/dac8311.h: 
+../application/global.h: 
+../application/audio_collect.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/grlib/grlib.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-msp430_18.12.4.LTS/include/assert.h: 
+../application/application.h: 
+../application/global.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/DSPLib/include/DSPLib.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/DSPLib/include/DSPLib_types.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/DSPLib/include/DSPLib_lea.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/DSPLib/include/DSPLib_support.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/DSPLib/include/DSPLib_vector.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/DSPLib/include/DSPLib_matrix.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/DSPLib/include/DSPLib_filter.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/DSPLib/include/DSPLib_transform.h: 
+C:/Users/Hjalti/workspace_v9/Igreypt_verkefni/DSPLib/include/DSPLib_utility.h: 
+../application/FFT.h: 
+../application/fir.h: 
