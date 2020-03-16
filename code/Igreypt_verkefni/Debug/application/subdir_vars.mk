8355c1f000034db63700761f5e5706d152e5619e
HjaltiThrastarson <62251615+HjaltiThrastarson@users.noreply.github.com>
2020-03-16 15:23:21 +0000

    Create subdir_vars.mk

---
+++ code/Igreypt_verkefni/Debug/application/subdir_vars.mk
@@ -0,0 +1,70 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+ASM_SRCS += \
+../application/FFT_430.asm 
+
+C_SRCS += \
+../application/FFT.c \
+../application/application.c \
+../application/audio_collect.c \
+../application/audio_playback.c \
+../application/benchmark.c \
+../application/dac8311.c 
+
+C_DEPS += \
+./application/FFT.d \
+./application/application.d \
+./application/audio_collect.d \
+./application/audio_playback.d \
+./application/benchmark.d \
+./application/dac8311.d 
+
+OBJS += \
+./application/FFT.obj \
+./application/FFT_430.obj \
+./application/application.obj \
+./application/audio_collect.obj \
+./application/audio_playback.obj \
+./application/benchmark.obj \
+./application/dac8311.obj 
+
+ASM_DEPS += \
+./application/FFT_430.d 
+
+OBJS__QUOTED += \
+"application\FFT.obj" \
+"application\FFT_430.obj" \
+"application\application.obj" \
+"application\audio_collect.obj" \
+"application\audio_playback.obj" \
+"application\benchmark.obj" \
+"application\dac8311.obj" 
+
+C_DEPS__QUOTED += \
+"application\FFT.d" \
+"application\application.d" \
+"application\audio_collect.d" \
+"application\audio_playback.d" \
+"application\benchmark.d" \
+"application\dac8311.d" 
+
+ASM_DEPS__QUOTED += \
+"application\FFT_430.d" 
+
+C_SRCS__QUOTED += \
+"../application/FFT.c" \
+"../application/application.c" \
+"../application/audio_collect.c" \
+"../application/audio_playback.c" \
+"../application/benchmark.c" \
+"../application/dac8311.c" 
+
+ASM_SRCS__QUOTED += \
+"../application/FFT_430.asm" 
+
+
